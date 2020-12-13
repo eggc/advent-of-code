2cf5b8530fd319e05a9ce9ece3bafbe2660b58d5
eguchi.ken <eguchi.ken@moneyforward.co.jp>
2020-12-14 00:56:10 +0900

    6個目の追加問題を解いた

--- 6.rb
+++ 6.rb
@@ -8,6 +8,16 @@ def solve_1
   solve('6.input.txt')
 end
 
+def solve_2
+  solve_other_version('6.input.txt')
+end
+
+def solve_other_version(input_file_name)
+  groups = read_groups(input_file_name)
+  yes_questions = groups.map { |group| find_everyone_yes_questions(group).size }
+  yes_questions.sum
+end
+
 def solve(input_file_name)
   groups = read_groups(input_file_name)
   yes_questions = groups.map { |group| find_yes_questions(group).size }
@@ -18,5 +28,18 @@ def find_yes_questions(lines)
   lines.join('').each_char.uniq
 end
 
+def find_everyone_yes_questions(lines)
+  # 最初の回答を基準にしておく
+  yes_questions = lines.first.split('')
+
+  lines.each do |line|
+    yes_questions = line.split('') & yes_questions
+  end
+
+  yes_questions
+end
+
 puts solve_sample
 puts solve_1
+puts solve_other_version('6.sample.txt')
+puts solve_2
