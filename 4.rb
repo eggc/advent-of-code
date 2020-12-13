3c771beed1697b5c4a397c9f2d58e29db379ce3c
eguchi.ken <eguchi.ken@moneyforward.co.jp>
2020-12-13 23:19:09 +0900

    4問目の追加問題を解いた

--- 4.rb
+++ 4.rb
@@ -2,6 +2,7 @@ require_relative "helper"
 
 class Passport
   REQUIRE_KEYS = %w(byr ecl eyr hcl hgt iyr pid)
+  VALID_EYE_COLORS = %w(amb blu brn gry grn hzl oth)
 
   def initialize(lines)
     @attributes = parse(lines)
@@ -20,9 +21,57 @@ class Passport
     (REQUIRE_KEYS - @attributes.keys).length == 0
   end
 
-  def to_s
-    format("[%5s] %s", valid?, @attributes.keys.sort.join(","))
+  def strict_valid?
+    validate_as_year(@attributes['byr'], 1920..2002) &&
+      validate_as_year(@attributes['iyr'], 2010..2020) &&
+      validate_as_year(@attributes['eyr'], 2020..2030) &&
+      validate_height &&
+      validate_hair_color &&
+      validate_eye_color &&
+      validate_passport_id
   end
+
+  def to_s
+    presence = valid?
+    valid = presence && strict_valid?
+
+    format("presence: %5s, valid: %5s, %s", presence, valid, @attributes)
+  end
+
+  private
+
+  def validate_as_year(year_string, range)
+    year = year_string.to_i
+    year_string.length == 4 && range.include?(year)
+  end
+
+  def validate_height
+    height_string = @attributes['hgt']
+    height = height_string.to_i
+
+    if height_string.end_with?('cm')
+      (150..193).include?(height)
+    elsif height_string.end_with?('in')
+      (59..76).include?(height)
+    else
+      false
+    end
+  end
+
+  def validate_hair_color
+    @attributes['hcl'].length == 7 &&
+      @attributes['hcl'].match?(/#[0-9a-f]{6}/)
+  end
+
+  def validate_eye_color
+    VALID_EYE_COLORS.include?(@attributes['ecl'])
+  end
+
+  def validate_passport_id
+    @attributes['pid'].length == 9 &&
+      @attributes['pid'].match?(/[0-9]{9}/)
+  end
+
 end
 
 def passports_from(input_file_name)
@@ -43,7 +92,7 @@ def passports_from(input_file_name)
   passports
 end
 
-def solve
+def solve1
   passports = passports_from("4.input.txt")
   passports.each_with_index do |passport, i|
     puts "#{i} #{passport.to_s}"
@@ -53,4 +102,19 @@ def solve
   puts "invalid: #{passports.count { |x| !x.valid? }}"
 end
 
-solve
+def solve2
+  passports = passports_from(ARGV[0] || "4.input.txt")
+
+  valid_count = passports.count do |passport|
+    passport.valid? && passport.strict_valid?
+  end
+
+  passports.each_with_index do |passport, i|
+    puts "#{i} #{passport.to_s}"
+  end
+
+  puts "valid: #{valid_count}"
+  puts "invalid: #{passports.count - valid_count}"
+end
+
+solve2
