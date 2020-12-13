88532239a5611ed04ae6e31f7b2dca7b37d455e3
eguchi.ken <eguchi.ken@moneyforward.co.jp>
2020-12-13 22:36:36 +0900

    4問目を解いた

--- 4.rb
+++ 4.rb
@@ -1,6 +1,8 @@
 require_relative "helper"
 
 class Passport
+  REQUIRE_KEYS = %w(byr ecl eyr hcl hgt iyr pid)
+
   def initialize(lines)
     @attributes = parse(lines)
   end
@@ -14,25 +16,41 @@ class Passport
     end
   end
 
+  def valid?
+    (REQUIRE_KEYS - @attributes.keys).length == 0
+  end
+
   def to_s
-    @attributes.to_s
+    format("[%5s] %s", valid?, @attributes.keys.sort.join(","))
   end
 end
 
 def passports_from(input_file_name)
   passports = []
-  lines = []
+  temporary_lines = []
+  lines = each_lines(input_file_name).map { |line| line }
+  lines << "" # 終端記号として空行を入れておく
 
-  each_lines(input_file_name) do |line|
+  lines.each_with_index do |line, i|
     if line.strip.empty?
-      passports << Passport.new(lines)
-      lines  = []
+      passports << Passport.new(temporary_lines)
+      temporary_lines  = []
     else
-      lines << line
+      temporary_lines << line
     end
   end
 
   passports
 end
 
-puts passports_from("4.sample.txt")
+def solve
+  passports = passports_from("4.input.txt")
+  passports.each_with_index do |passport, i|
+    puts "#{i} #{passport.to_s}"
+  end
+
+  puts "valid: #{passports.count(&:valid?)}"
+  puts "invalid: #{passports.count { |x| !x.valid? }}"
+end
+
+solve
